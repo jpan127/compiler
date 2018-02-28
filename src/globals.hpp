65938bed25d84a2db62265a6c8495dee136f3559
Jonathan Pan <jpan127@users.noreply.github.com>
2018-02-27 22:08:21 -0800

    Skeleton : adding intermediate code and symbol table

--- src/globals.hpp
+++ src/globals.hpp
@@ -5,7 +5,29 @@
 #include <stdint>
 #include <cstdlib>
 
+/// Only import from namespace std what are neccessary
 using namespace std::string;
+using namespace std::vector;
+using namespace std::map;
 
-#define MIN(a, b)   ((a > b) ? (b) : (a))
-#define MAX(a, b)   ((a < b) ? (b) : (a))
+/// Max / Min helpers 
+template <T> static inline T MAX(T a, T b) { return ((a > b) ? (b) : (a)); }
+template <T> static inline T MIN(T a, T b) { return ((a < b) ? (b) : (a)); }
+
+/**
+ *  Generic struct that is meant to contain either a string or an integer, or any type
+ *  The value can only be set on construction
+ */
+template <T>
+typedef struct data_s
+{
+public:
+
+    /// Constructor
+    data_s(const T value) : value(value) { }
+
+private:
+
+    const T value;
+
+} data_S;
